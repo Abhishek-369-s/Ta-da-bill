8ee8913b2ba075ff6fb483d87f9fb62492a36685
AbhishekAB <127830836+Abhishek-369-s@users.noreply.github.com>
2024-07-22 16:18:14 +0000

    did some expt

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+name='kothali'
 
 @app.route("/")
 def hello():
-  return render_template('home.html')
+  return render_template('home.html',name=name)
  
 
 if __name__ == "__main__":
